e0197215db6c54c7f8749eefcfe035eaf82546d9
andres.arevalopar <andres.arevalopar@outlook.es>
2019-02-14 20:12:49 -0500

    Project console, define initial scenary

--- SkiResortRoute/Program.cs
+++ SkiResortRoute/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,20 @@ namespace SkiResortRoute
     {
         static void Main(string[] args)
         {
+            int[][] matrix = new int[][]
+            {
+                new int[] { 4, 4},
+                new int[] { 4, 8, 7, 3 },
+                new int[] { 2, 5, 9, 3 },
+                new int[] { 6, 3, 2, 5 },
+                new int[] { 4, 4, 1, 6 }
+            };
+
+            // Read the file as one string.
+            StreamReader skiingFile = new StreamReader("");
+            string myString = skiingFile.ReadToEnd();
+            
+
         }
     }
 }
