3af51de3f5daffe23ab4f0eb0ad87df14e7b0e08
andres.arevalopar <andres.arevalopar@outlook.es>
2019-02-15 01:02:08 -0500

    Read initial file and define reading structure

--- SkiResortRoute/Program.cs
+++ SkiResortRoute/Program.cs
@@ -11,20 +11,71 @@ namespace SkiResortRoute
     {
         static void Main(string[] args)
         {
-            int[][] matrix = new int[][]
+            string pathFile = "C:/Users/Andres Arevalo/source/repos/SkiResortRoute/SkiResortRoute/4x4.txt";
+
+            // 1. Read the file as one string.
+            Tuple<int, int, List<int>, bool> fileRead = ReadAndDecodeFile(pathFile);
+
+
+        }
+
+        /// <summary>
+        /// Reads the and decode file.
+        /// </summary>
+        /// <param name="pathFile">The path file.</param>
+        /// <returns></returns>
+        private static Tuple<int, int, List<int>, bool> ReadAndDecodeFile(string pathFile)
+        {
+            var result = Tuple.Create(0, 0, new List<int>(), false);
+
+            using (StreamReader skiingFile = new StreamReader(pathFile))
             {
-                new int[] { 4, 4},
-                new int[] { 4, 8, 7, 3 },
-                new int[] { 2, 5, 9, 3 },
-                new int[] { 6, 3, 2, 5 },
-                new int[] { 4, 4, 1, 6 }
-            };
+                string[] firstLine = skiingFile.ReadLine().Split(' ');
+                
+                if (firstLine.Count() != 2)
+                    return result;
 
-            // Read the file as one string.
-            StreamReader skiingFile = new StreamReader("");
-            string myString = skiingFile.ReadToEnd();
-            
+                int rowsNumber = Convert.ToInt32(firstLine.First());
+                int columnsNumber = Convert.ToInt32(firstLine.Last());
+                int dimensions = (rowsNumber * columnsNumber);
 
+                var contentFileRaw = skiingFile.ReadToEnd().Replace(" ", string.Empty)
+                                                           .Replace("\r", string.Empty)
+                                                           .Split('\n').ToList();
+
+                var fileDecode = ReadFile(contentFileRaw);
+
+                if (fileDecode?.Any() != true)
+                    return result;
+
+                bool convertStatus = (fileDecode.Count == dimensions) ? true: false;
+
+                result = Tuple.Create(rowsNumber, columnsNumber, fileDecode, convertStatus);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Reads the file.
+        /// </summary>
+        /// <param name="contentFileRaw">The content file raw.</param>
+        /// <returns></returns>
+        private static List<int> ReadFile(List<string> contentFileRaw)
+        {
+            List<int> contentFile = new List<int>();
+
+            try
+            {
+                foreach (var row in contentFileRaw)
+                    row.ToList().ForEach(x => contentFile.Add(Convert.ToInt32(x.ToString())));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return contentFile;
         }
     }
 }
