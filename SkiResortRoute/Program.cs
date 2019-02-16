c403c6f81b22f70120db8d208931bce8cde4d321
andres.arevalopar <andres.arevalopar@outlook.es>
2019-02-15 20:02:21 -0500

    Functionality complete with static values and files

--- SkiResortRoute/Program.cs
+++ SkiResortRoute/Program.cs
@@ -2,21 +2,74 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SkiResortRoute
 {
     class Program
     {
+        /// <summary>
+        /// The rows count
+        /// </summary>
         private static int rowsCount;
+
+        /// <summary>
+        /// The columns count
+        /// </summary>
         private static int columnsCount;
 
+        /// <summary>
+        /// Navigation Class
+        /// </summary>
+        public class Navigation
+        {
+            /// <summary>
+            /// Gets or sets the path lenght.
+            /// </summary>
+            /// <value>
+            /// The path lenght.
+            /// </value>
+            public int PathLenght { get; set; }
+
+            /// <summary>
+            /// Gets or sets the nav coords.
+            /// </summary>
+            /// <value>
+            /// The nav coords.
+            /// </value>
+            public NavigationCoordinates NavCoords { get; set; }
+        }
+
+        /// <summary>
+        /// Navigation Coodinates Class
+        /// </summary>
+        public class NavigationCoordinates
+        {
+            /// <summary>
+            /// Gets or sets the x coord.
+            /// </summary>
+            /// <value>
+            /// The x coord.
+            /// </value>
+            public int xCoord { get; set; }
+
+            /// <summary>
+            /// Gets or sets the y coord.
+            /// </summary>
+            /// <value>
+            /// The y coord.
+            /// </value>
+            public int yCoord { get; set; }
+        }
+
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            string pathFile = "C:/Users/Andres Arevalo/source/repos/SkiResortRoute/SkiResortRoute/map.txt";
+            string pathFile = "C:/Users/Andres Arevalo/source/repos/SkiResortRoute/SkiResortRoute/4x4.txt";
 
-            // 1. Read and define GeoMap array from file and the differents Paths and Drops into arrays.
+            #region 1. Read and define GeoMap array from file and the differents Paths and Drops into arrays.
             Tuple<int, int, int[,], bool> fileRead = ReadAndDecodeFile(pathFile);
 
             if (fileRead.Item4 == false) { Console.WriteLine("File malformed"); return; }
@@ -27,8 +80,9 @@ namespace SkiResortRoute
             int[,] GeoMap = fileRead.Item3;
             int[,] Path = new int[rowsCount, columnsCount];
             int[,] Drop = new int[rowsCount, columnsCount];
+            #endregion
 
-            // 2. Apply DFS algorithm
+            #region 2. Apply DFS algorithm
             for (int i = 0; i < rowsCount; i++)
             {
                 for (int j = 0; j < columnsCount; j++)
@@ -38,23 +92,63 @@ namespace SkiResortRoute
                     Drop[i, j] = GeoMap[i, j] - GeoMap[PathAux.NavCoords.xCoord, PathAux.NavCoords.yCoord];
                 }
             }
+            #endregion
 
-            // 3. Get Coordinates of point with max path and drop in the Path[] and Drop[]
+            #region 3. Get Coordinates of point with max path and drop in the Path[] and Drop[]
 
-            int[] maxPathCoordXY = new int[2]; // Coordinate X,Y of point with max path and drop
-            int maxPath = -1;                  // Have the longest path
-            int maxDrop = -1;                  // Have the longest Drop
+               int[] maxPathCoordXY = new int[2]; // Coordinate X,Y of point with max path and drop
+               int maxPath = -1;                  // Have the longest path
+               int maxDrop = -1;                  // Have the longest Drop
 
             // 3.1 Find maxPath and maxDrop 
                GetMaxPath(Path, Drop, maxPathCoordXY, ref maxPath, ref maxDrop);
+            #endregion
 
+            #region 4. Print Maximum Path and Drop
+                Console.WriteLine($"Maximal Path is: {maxPath} \nMaximal Drop is: {maxDrop}");
+            #endregion
 
-            // 4. Print Maximum Path
-               Console.WriteLine($"Maximal Path is: {maxPath} \nMaximal Drop is: {maxDrop}");
+            #region 5. Print all Path points from the maximum to the minimum Point (Skiing down)
 
-               Console.ReadLine();
+               // Load respective CoordX and CoordY values
+               List<int> listPath = DFSForMaxPathLength(maxPathCoordXY[0], maxPathCoordXY[1], fileRead.Item3);
+               listPath.Reverse(); 
+
+               Console.WriteLine($"\nThe Waypoints are: \n\n | XCoord | YCoord | SkiElevation |");
+
+               int[] CoordXY = new int[2];
+               int index = 0;
+               int temp = 0;
+               string printResult = string.Empty;
+
+               foreach (var aux in listPath)
+               {
+                    CoordXY[index % 2] = aux;
+                    index++;
+               
+                    if(index % 2 == 0)
+                    {
+                       printResult += string.Format(" |  {0}   |  {1}   | Point value: {2}| \n", temp, aux, 
+                                                                                                 fileRead.Item3[CoordXY[0], CoordXY[1]]);    
+                    }
+
+                    temp = aux;
+               }
+
+               Console.WriteLine(printResult);
+            #endregion
+
+            Console.ReadLine();
         }
 
+        /// <summary>
+        /// Gets the maximum path.
+        /// </summary>
+        /// <param name="Path">The path.</param>
+        /// <param name="Drop">The drop.</param>
+        /// <param name="maxPathCoordXY">The maximum path coord xy.</param>
+        /// <param name="maxPath">The maximum path.</param>
+        /// <param name="maxDrop">The maximum drop.</param>
         private static void GetMaxPath(int[,] Path, int[,] Drop, int[] maxPathCoordXY, ref int maxPath, ref int maxDrop)
         {
             for (int i = 0; i < rowsCount; i++)
@@ -156,18 +250,6 @@ namespace SkiResortRoute
             return contentFile;
         }
 
-        public class Navigation
-        {
-            public int PathLenght { get; set; }
-            public NavigationCoordinates NavCoords { get; set; }
-        }
-
-        public class NavigationCoordinates
-        {
-            public int xCoord { get; set; }
-            public int yCoord { get; set; }
-        }
-
         /// <summary>
         /// DFSs the operation.
         /// </summary>
@@ -223,6 +305,54 @@ namespace SkiResortRoute
             pathAndDrop.PathLenght++;
 
             return pathAndDrop;
+        }
+
+        /// <summary>
+        /// DFSs the length of for maximum path.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="GeoMap">The geo map.</param>
+        /// <returns></returns>
+        public static List<int> DFSForMaxPathLength(int x, int y, int[,] GeoMap)
+        {
+            List<int> list = new List<int>();
+            List<int> currentPathList = new List<int>();
+
+            // Searching UP ▲ Direction
+            if (y > 0 && GeoMap[x , y] > GeoMap[x , y - 1])
+            {
+                currentPathList = DFSForMaxPathLength(x, y - 1, GeoMap);
+                if (currentPathList.Count > list.Count)
+                    list = currentPathList;
+            }
+
+            // Searching DOWN ▼ Direction
+            if (y < (columnsCount - 1) && GeoMap[x, y] > GeoMap[x , y + 1])
+            {
+                currentPathList = DFSForMaxPathLength(x, y + 1, GeoMap);
+                if (currentPathList.Count > list.Count)
+                    list = currentPathList;
+            }
+
+            // Searching LEFT ◄ Direction
+            if (x > 0 && GeoMap[x , y] > GeoMap[x - 1 , y])
+            {
+                currentPathList = DFSForMaxPathLength(x - 1, y, GeoMap);
+                if (currentPathList.Count > list.Count)
+                    list = currentPathList;
+            }
+
+            // Searching RIGHT ► Direction
+            if (x < (rowsCount - 1) && GeoMap[x ,y] > GeoMap[x + 1 ,y])
+            {
+                currentPathList = DFSForMaxPathLength(x + 1, y, GeoMap);
+                if (currentPathList.Count > list.Count)
+                    list = currentPathList;
+            }
+
+            list.Add(y); list.Add(x); 
+            return list;
         }
     }
 }
